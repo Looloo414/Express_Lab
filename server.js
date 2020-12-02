b3dd22aa6b6f4e219411fc5813264583cf1f6279
Looloo414 <alexafaulkison@gmail.com>
2020-12-01 22:46:16 -0700

    Fixed initial render errors

--- server.js
+++ server.js
@@ -6,37 +6,35 @@ const goalDb = require('./data/goal-db');
 const app = express()   //create express app
 
 // Configure the app (app.set)
-app.set('view engine', 'ejs');›4›
+app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views'));
-	
+
 // Mount middleware (app.use)
-	
-	
+
+
 // Mount routes
-app.get('/', function(req, res) {
-    res.redirect('/goals')
+app.get('/', function (req, res) {
+  res.redirect('/goals')
 })
 
 // Define a "root" route directly on app
 app.get('/', function (req, res) {
-    // res.redirect('/home')
-    res.send('<h1>Alexas Goals</h1>');
-  });
+  res.redirect('/home')
+  res.send('<h1>Alexas Goals</h1>');
+});
 
-  app.get('/home', function (req, res){
-    res.render('home')
+app.get('/home', function (req, res) {
+  res.render('home')
 })
 
-app.get('/goals', function(req, res) {
-    res.render('goals/index', {
-      goals: goalDb.getAll()
-    });
+app.get('/goals', function (req, res) {
+  res.render('goals/index', {
+    goals: goalDb.getAll()
   });
-  
-  // Tell the app to listen on port 3000
-  // for HTTP requests from clients
-  app.listen(3000, function () {
-    console.log('Listening on port 3000');
-  });
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
 
 
